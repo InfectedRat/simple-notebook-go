1295d6c621c9606761d77306f5056631ce20d199
Maksim Babichev <birdclock@gmail.com>
2024-08-22 00:11:40 +0300

    add simple interface

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,7 +7,7 @@ import (
 
 	logic "simple-notebook-go/internal/app"
 	database "simple-notebook-go/internal/database"
-	transport "simple-notebook-go/internal/transport"
+	"simple-notebook-go/internal/transport"
 )
 
 func main() {
@@ -22,8 +22,17 @@ func main() {
 		fmt.Printf("ID: %d, Title: %s, Content: %s\n", note.ID, note.Title, note.Content)
 	}
 
+	// http.HandleFunc("/notes", transport.NoteHandler(db))
+	// http.HandleFunc("/getnotes", transport.GetAllNotesHandler(db))
+
+	// API маршруты
 	http.HandleFunc("/notes", transport.NoteHandler(db))
-	http.HandleFunc("/getnotes", transport.GetAllNotesHandler(db))
+	http.HandleFunc("/allnotes", transport.GetAllNotesHandler(db))
+
+	// Обслуживаем статические файлы (HTML, CSS, JS)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
 	fmt.Println("Сервер запущен на порту 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
